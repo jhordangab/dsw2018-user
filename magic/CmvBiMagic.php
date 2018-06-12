a08d3235bbad19f9b88be2ad0d2bc9e09aeb7dbc
Jhordan <jhordangabriel@hotmail.com>
2018-06-12 14:11:53 -0300

    recorrecao cmv

--- magic/CmvBiMagic.php
+++ magic/CmvBiMagic.php
@@ -32,38 +32,6 @@ class CmvBiMagic
                 valor18 as dez, 
                 valor19 as total
             FROM indicador3
-            UNION ALL
-            SELECT
-                valor1 as empresa,
-                valor2 as ano,
-                '' as categoria,
-                '' as codigo,
-                'ATIVO CIRCULANTE' as descricao,
-                (SELECT SUM(valor6) FROM indicador14 i2 WHERE i2.valor1 = cs.valor1 AND i2.valor2 = cs.valor2 AND i2.valor21 = cs.valor21 AND i2.valor20 = cs.valor20) as jan,
-                (SELECT SUM(valor7) FROM indicador14 i2 WHERE i2.valor1 = cs.valor1 AND i2.valor2 = cs.valor2 AND i2.valor21 = cs.valor21 AND i2.valor20 = cs.valor20) as feb,
-                (SELECT SUM(valor8) FROM indicador14 i2 WHERE i2.valor1 = cs.valor1 AND i2.valor2 = cs.valor2 AND i2.valor21 = cs.valor21 AND i2.valor20 = cs.valor20) as mar,
-                (SELECT SUM(valor9) FROM indicador14 i2 WHERE i2.valor1 = cs.valor1 AND i2.valor2 = cs.valor2 AND i2.valor21 = cs.valor21 AND i2.valor20 = cs.valor20) as apr,
-                (SELECT SUM(valor10) FROM indicador14 i2 WHERE i2.valor1 = cs.valor1 AND i2.valor2 = cs.valor2 AND i2.valor21 = cs.valor21 AND i2.valor20 = cs.valor20) as may,
-                (SELECT SUM(valor11) FROM indicador14 i2 WHERE i2.valor1 = cs.valor1 AND i2.valor2 = cs.valor2 AND i2.valor21 = cs.valor21 AND i2.valor20 = cs.valor20) as jun,
-                (SELECT SUM(valor12) FROM indicador14 i2 WHERE i2.valor1 = cs.valor1 AND i2.valor2 = cs.valor2 AND i2.valor21 = cs.valor21 AND i2.valor20 = cs.valor20) as jul,
-                (SELECT SUM(valor13) FROM indicador14 i2 WHERE i2.valor1 = cs.valor1 AND i2.valor2 = cs.valor2 AND i2.valor21 = cs.valor21 AND i2.valor20 = cs.valor20) as aug,
-                (SELECT SUM(valor14) FROM indicador14 i2 WHERE i2.valor1 = cs.valor1 AND i2.valor2 = cs.valor2 AND i2.valor21 = cs.valor21 AND i2.valor20 = cs.valor20) as sep,
-                (SELECT SUM(valor15) FROM indicador14 i2 WHERE i2.valor1 = cs.valor1 AND i2.valor2 = cs.valor2 AND i2.valor21 = cs.valor21 AND i2.valor20 = cs.valor20) as oct,
-                (SELECT SUM(valor16) FROM indicador14 i2 WHERE i2.valor1 = cs.valor1 AND i2.valor2 = cs.valor2 AND i2.valor21 = cs.valor21 AND i2.valor20 = cs.valor20) as nov,
-                (SELECT SUM(valor17) FROM indicador14 i2 WHERE i2.valor1 = cs.valor1 AND i2.valor2 = cs.valor2 AND i2.valor21 = cs.valor21 AND i2.valor20 = cs.valor20) as dez,
-                '' as total
-            FROM
-            (
-                SELECT 
-                    valor1,
-                    valor2,
-                    valor3,
-                    valor20, 
-                    valor21
-                FROM agrocontar.indicador14
-                WHERE valor20 = '1.1'
-                GROUP BY valor1, valor2, valor20, valor21, valor3
-            ) AS cs
         ) as sel
         WHERE sel.ano = {$ano}
         AND sel.empresa = {$empresa_id}
