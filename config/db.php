88421e470f33af82c1c9407a6172887914e782f4
Jhordan <jhordangabriel@hotmail.com>
2018-06-18 16:20:07 -0300

    db

--- config/db.php
+++ config/db.php
@@ -3,8 +3,8 @@
 return 
 [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=127.0.0.1;dbname=agrocontar',
-    'username' => 'muser',
-    'password' => 'r44t',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=agrocontar_atualizado',
+    'username' => 'root',
+    'password' => 'root',
     'charset' => 'utf8'
 ];
